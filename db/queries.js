6406602f87fc886433c6352f1ba5f77235ac3a88
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-11-20 02:41:26 -0700

    Showing one record with GET /api/v1/stickers/:id by creating route, validating id, creating querie and adding test

--- db/queries.js
+++ db/queries.js
@@ -4,4 +4,7 @@ module.exports = {
  getAll() {
    return knex('sticker');
  },
+ getOne(id) {
+   return knex('sticker').where('id', id).first();
+ },
 }
--- test/app.test.js
+++ test/app.test.js
@@ -24,4 +24,17 @@ describe('CRUD Stickers', () => {
         done();
       });
   });
+
+  it('Show one record by id', (done) => {
+    request(app)
+      .get('/api/v1/stickers/5')
+      .set('Accept', 'application/json')
+      .expect('Content—Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a('object');
+        expect(response.body).to.deep.equal(fixtures.stickers[4]);
+        done();
+      });
+  });
 });
