09cfc0be62a8b14e16f66a933c54562986c56adf
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-11-20 02:37:02 -0700

    Created first test, msde fixtures file to use for testing,

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,27 @@
-describe('CRUD Stickers', () => {
+const request = require('supertest');
+const chai = require('chai').expect;
+const knex = require('../db/knex');
+const app = require('../app');
+const fixtures = require('./fixtures');
 
+describe('CRUD Stickers', () => {
+  before((done) => {
+    knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run();
+      }).then(() => done());
+  });
+
+  it('Lists all Records', (done) => {
+    request(app)
+      .get('/api/v1/stickers')
+      .set('Accept', 'application/json')
+      .expect('Content—Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a('array');
+        expect(response.body).to.deep.equal(fixtures.stickers);
+        done();
+      });
+  });
 });
