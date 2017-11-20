b7def34a6fd14fd2eb22fd45b47a8b389145f1fa
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-11-20 01:00:12 -0700

    Created routes folder, in routes folder created a simple get route, imported and mounted router in app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app = express();
 
-var app = express();
+const stickers = require('./api/stickers');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -14,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
+app.use('/api/v1/stickers', stickers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
