c2cd29cfaa4de7ca79162d12d9ec75bfb016ed15
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-11-20 01:40:44 -0700

    installed mocha chai and supertest, created test file and checked that test was running

--- knexfile.js
+++ knexfile.js
@@ -6,4 +6,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/quintanas-web-store'
   },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/test-quintanas-web-store'
+  },
 };
---
+++ test/app.test.js
@@ -0,0 +1,3 @@
+describe('CRUD Stickers', () => {
+
+});
